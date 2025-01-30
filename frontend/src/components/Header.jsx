968e8323c2a6c5956a8e460f9106e93b3bf59bc7
Denis Moini <denismoini09@gmail.com>
2025-01-31 02:43:18 +0300

    bootstrapped app

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -26,63 +26,21 @@ const Header = () => {
       <Navbar className="bg-body-tertiary" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>Fantasy UG</Navbar.Brand>
+            <Navbar.Brand>SerieA lounge</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              {userInfo && userInfo?.roles?.ADMIN && (
-                <LinkContainer to="/admin/dashboard">
-                  <Nav.Link>Dashboard</Nav.Link>
-                </LinkContainer>
-              )}
-              {userInfo?.roles?.NORMAL_USER && !userInfo?.hasPicks && 
-              <LinkContainer to="/create">
-              <Nav.Link>Create Team</Nav.Link>
-            </LinkContainer>}
-              
-                  {
-                    userInfo?.hasPicks && (
-                      <>
-                        <LinkContainer to="/points">
-                          <Nav.Link>Points</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/pickteam">
-                          <Nav.Link>Pick Team</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/transfers">
-                          <Nav.Link>Transfers</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/userleagues">
-                          <Nav.Link>Leagues</Nav.Link>
-                        </LinkContainer>
-                      </>
-                    )}
-              {userInfo ? (
-                <>
-                  <NavDropdown title={userInfo.name} id="username">
-                    <LinkContainer to="/profile">
-                      <NavDropdown.Item>Profile</NavDropdown.Item>
-                    </LinkContainer>
-                    <NavDropdown.Item onClick={logoutHandler}>
-                      Logout
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </>
-              ) : (
-                <>
-                  <LinkContainer to="/login">
-                    <Nav.Link>
-                      <FaSignInAlt /> Sign In
-                    </Nav.Link>
-                  </LinkContainer>
-                  <LinkContainer to="/register">
-                    <Nav.Link>
-                      <FaSignOutAlt /> Sign Up
-                    </Nav.Link>
-                  </LinkContainer>
-                </>
-              )}
+              <LinkContainer to="/admin">
+                <Nav.Link>Admin</Nav.Link>
+              </LinkContainer>
+              <>
+                <NavDropdown id="username">
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </>
             </Nav>
           </Navbar.Collapse>
         </Container>
